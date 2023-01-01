6182e4109be8c3e1c44e9b1932aad3432142fe01
OLGA DRACH <dolchik88@mail.ru>
2023-01-01 19:57:43 +0300

    Двумерный массив, созданный рандомно

---
+++ Ex_01/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+// 01:08:33
+
+void PrintArray(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+{
+
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// получить от пользователя m & n
+int m = EnterNumber("Введите значение m: ");
+int n = EnterNumber("Введите значение m: ");
+// создать и заполнить двумерный массив
+int[,] matrix = CreateRandomArray(m, n, 0, 10);
+// вывод массив в консоль
+PrintArray(matrix);
