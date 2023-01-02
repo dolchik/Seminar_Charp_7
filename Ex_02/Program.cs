eee8fc730f5921164f2e4bd518b4ca2320b854c3
OLGA DRACH <dolchik88@mail.ru>
2023-01-02 11:41:17 +0300

    Двумерный массив

---
+++ Ex_02/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+//каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+//Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void PrintArray(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = i + j;
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+//получить от пользователя m & n
+int m = EnterNumber("Введите количество строк: ");
+int n = EnterNumber("Введите количество столбцов: ");
+//задать массив 
+int[,] matrix = CreateArray(m, n);
+// вывести массив в консоль
+PrintArray(matrix);
+
+
+
